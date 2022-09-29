5c145542c87cc64fde1252e249bad78a76e643e4
hifilorau <tomrau@gmail.com>
2022-09-28 18:23:52 -0600

    checkin

--- components/HeaderContent.js
+++ components/HeaderContent.js
@@ -11,15 +11,17 @@ const HeaderContent = () => {
         <p>You may well be among the many believing that it easier to imagine the end of the world than the end of capitalism (ironically, those two concepts may be related according to some).
         </p>
         <div className={styles.quoteBlock}>
-          {/* <div className={styles.vertLine}></div> */}
-          <p>
-            The important news is that we have no choice but to develop alternatives, because capitalism is dying.
-          </p>
-          <p>
-            The bad news is that it is going to be long, tough ride for at least the next several years, if not longer, and a “good” future is, by no means guaranteed.
-          </p>
-          <p>The good news is that there are indeed viable guiding images of life after capitalism.
-          </p>
+          <div className={styles.vertLine}></div>
+          <div>
+            <p>
+              The important news is that we have no choice but to develop alternatives, because capitalism is dying.
+            </p>
+            <p>
+              The bad news is that it is going to be long, tough ride for at least the next several years, if not longer, and a “good” future is, by no means guaranteed.
+            </p>
+            <p>The good news is that there are indeed viable guiding images of life after capitalism.
+            </p>
+          </div>
         </div>
        
         <p>You may well be among the many believing that it easier to imagine the end of the world than the end of capitalism (ironically, those two concepts may be related according to some).As a futurist, it's my job to help craft meaningful images of the future that provide a guiding ideal that makes embarking on the long journey worth it. I chronicle the most pressing problems of the day to show it is time for a new conception of the future, then outline a pathway to three positive guiding images of a transformed society. The images developed for this work each focus on a different aspect of society —ecological preservation, socio-economic justice, and technological metamorphosis. We will refer to these positive images as Circular Commons, Tech-Led Abundance, and Non-Workers Paradise. In this book we will explore the possibilities these new images of the future offer and provide you with a glimpse into what After Capitalism might look like.
