4c7e448792946ad21f082370127eabacae73f915
Tom Rau <tomrau@gmail.com>
2023-08-06 17:32:24 -0400

    checkin

--- components/footer.js
+++ components/footer.js
@@ -22,12 +22,10 @@ const Footer = () => {
       href="/faqs"
     >FAQs
     </Link>
-    <a
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
+    <Link
+      href="/bibliography"
     >Media Links
-    </a>
+    </Link>
   </footer>)
 }
 
